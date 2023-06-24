69a1d1e1b78d6c510259c02a057072058a184906
lidongqing1 <lidongqing1@tal.com>
2023-06-24 23:42:01 +0800

    返回结果格式化

--- internal/server/http.go
+++ internal/server/http.go
@@ -6,6 +6,7 @@ import (
 	"cdncloud/internal/conf"
 	"cdncloud/internal/service"
 	"cdncloud/internal/service/api"
+	"encoding/json"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
@@ -15,6 +16,7 @@ import (
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, userService *api.UserService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
+		http.ResponseEncoder(responseEncoder),
 		http.Middleware(
 			recovery.Recovery(),
 		),
@@ -32,4 +34,24 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, userService 
 	v1.RegisterGreeterHTTPServer(srv, greeter)
 	v1User.RegisterUserHTTPServer(srv, userService)
 	return srv
+}
+
+func responseEncoder(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	type Response struct {
+		Code    int         `json:"code"`
+		Message string      `json:"message"`
+		Data    interface{} `json:"data"`
+	}
+	res := &Response{
+		Code:    200,
+		Message: "success",
+		Data:    data,
+	}
+	msRes, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(msRes)
+	return nil
 }
